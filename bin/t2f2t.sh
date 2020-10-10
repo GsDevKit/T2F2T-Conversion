4be13f2a6f770f9255be7206ceb03c3ac5f05929
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2020-10-10 08:04:26 -0700

    help is generated from a help file on disk (easier to edit file) and less is used to display help message (necessary for long man page)

--- bin/t2f2t.sh
+++ bin/t2f2t.sh
@@ -5,7 +5,16 @@
 export binDir="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 export GEMSTONE="$binDir/gs_350/GemStone64Bit3.5.0-x86_64.Linux"
 
-$GEMSTONE/bin/topaz -i -l -q -C "GEM_SOLO_EXTENT=$binDir/gs_350/extent0.rowan_350.dbf" -S $binDir/t2f2t.tpz -- $*
+
+
+if [ "$*" = "-h" ] || [ "$*" = "--help" ] ; then
+	helpfile=$(mktemp)
+	$GEMSTONE/bin/topaz -i -l -q -C "GEM_SOLO_EXTENT=$binDir/gs_350/extent0.rowan_350.dbf" -S $binDir/t2f2t.tpz -- --help > $helpfile
+	less $helpfile
+else
+	$GEMSTONE/bin/topaz -i -l -q -C "GEM_SOLO_EXTENT=$binDir/gs_350/extent0.rowan_350.dbf" -S $binDir/t2f2t.tpz -- $*
+fi
+
 
 if [ $? != 0 ]; then 
   echo "ERRORs from topaz"
