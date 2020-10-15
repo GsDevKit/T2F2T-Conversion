82fd646193e8bd2b81f3031ee0b155d616b73aa5
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2020-10-14 18:02:00 -0700

    get rid of wired paths (finally found them) depend on ROWAN_PROJECTS_HOME being optionally defined

--- bin/t2f2t.sh
+++ bin/t2f2t.sh
@@ -5,6 +5,10 @@
 export binDir="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 export GEMSTONE="$binDir/gs_350/GemStone64Bit3.5.0-x86_64.Linux"
 
+if [ "$ROWAN_PROJECTS_HOME"x = "x" ] ; then
+	export ROWAN_PROJECTS_HOME="$binDir/.."
+fi
+
 if [ "$*" = "-h" ] || [ "$*" = "--help" ] ; then
 	helpfile=$(mktemp)
 	$GEMSTONE/bin/topaz -i -l -q -C "GEM_SOLO_EXTENT=$binDir/gs_350/extent0.rowan_350.dbf" -S $binDir/t2f2t.tpz -- --help > $helpfile
